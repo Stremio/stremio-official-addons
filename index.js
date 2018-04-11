24df223fd03fd98facbaebb460a618c2789fd3b0
Ivo Georgiev <ivo@strem.io>
2018-04-11 12:08:38 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,141 @@
+[
+    {
+        "manifest": {
+            "id": "com.linvo.cinemeta",
+            "name": "Cinemeta",
+            "description": "The official add-on for info from IMDB, TheTVDB, TheMovieDB and Fanart.tv",
+            "resources": [
+                "meta"
+            ],
+            "types": [
+                "series",
+                "movie"
+            ],
+            "catalogs": [
+                {
+                    "type": "series",
+                    "id": "top"
+                },
+                {
+                    "type": "movie",
+                    "id": "top"
+                }
+            ],
+            "url": "https://cinemeta.strem.io/stremioget/stremio/v1",
+            "transport": "legacy",
+            "idPrefixes": [
+                "tt"
+            ]
+        },
+        "transportName": "legacy",
+        "flags": {
+            "official": true
+        }
+    },
+    {
+        "manifest": {
+            "id": "com.linvo.stremiochannels",
+            "name": "YouTube",
+            "description": "Watch your favourite YouTube channels ad-free and get notified when they upload new videos.",
+            "logo": "http://www.strem.io/images/addons/youtube-logo.png",
+            "background": "http://img0.mxstatic.com/wallpapers/b00ab97a4504c647bda61f53a6b5bed8_large.jpeg",
+            "resources": [
+                "meta",
+                "stream"
+            ],
+            "types": [
+                "channel"
+            ],
+            "catalogs": [
+                {
+                    "type": "channel",
+                    "id": "popularity"
+                },
+                {
+                    "type": "channel",
+                    "id": "popularities.local"
+                }
+            ],
+            "url": "https://channels.strem.io/stremioget/stremio/v1",
+            "transport": "legacy",
+            "idPrefixes": [
+                "UC"
+            ]
+        },
+        "transportName": "legacy",
+        "flags": {
+            "official": true
+        }
+    },
+    {
+        "manifest": {
+            "id": "org.stremio.netflix",
+            "name": "Netflix",
+            "description": "Watch Netflix movies and series",
+            "resources": [
+                "stream"
+            ],
+            "types": [
+                "movie",
+                "series"
+            ],
+            "catalogs": [
+                {
+                    "type": "movie",
+                    "id": "popularities.netflix"
+                },
+                {
+                    "type": "series",
+                    "id": "popularities.netflix"
+                }
+            ],
+            "url": "https://nfxaddon.strem.io/stremioget/stremio/v1",
+            "transport": "legacy",
+            "idPrefixes": [
+                "tt",
+                "netflix_id:"
+            ]
+        },
+        "transportName": "legacy",
+        "flags": {
+            "official": true
+        }
+    },
+    {
+        "manifest": {
+            "id": "org.stremio.guidebox",
+            "name": "WatchHub",
+            "description": "Find where to stream your favorite movies and shows amongst iTunes, Hulu, Amazon and other UK/US services.",
+            "contactEmail": "office@strem.io",
+            "logo": "http://www.strem.io/images/watchhub-logo.png",
+            "resources": [
+                "meta",
+                "stream"
+            ],
+            "types": [
+                "movie",
+                "series"
+            ],
+            "catalogs": [
+                {
+                    "type": "channel",
+                    "id": "popularities.guidebox"
+                },
+                {
+                    "type": "movie",
+                    "id": "popularities.guidebox_free"
+                }
+            ],
+            "url": "https://watchhub.strem.io/stremioget/stremio/v1",
+            "transport": "legacy",
+            "idPrefixes": [
+                "guidebox_id:",
+                "tt"
+            ]
+        },
+        "transportName": "legacy",
+        "flags": {
+            "official": true
+        }
+    }
+]

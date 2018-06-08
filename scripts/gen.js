3959424ee541a5bc09ee055bdf950bcd3fc3b3bf
Ivo Georgiev <ivo@strem.io>
2018-06-08 23:32:50 +0300

    scripts/gen: flags implemented, add local addon

--- scripts/gen.js
+++ scripts/gen.js
@@ -7,13 +7,20 @@ const ENDPOINTS = [
     "https://v3-channels.strem.io/manifest.json",
     "https://nfxaddon.strem.io/stremioget/stremio/v1",
     "https://watchhub.strem.io/stremioget/stremio/v1",
-    "https://opensubtitles.strem.io/stremio/v1"
+    "https://opensubtitles.strem.io/stremio/v1",
+    "http://127.0.0.1:1222/manifest.json",
 ]
 
 const col = new client.AddonCollection()
 
 Promise.all(ENDPOINTS.map(url => client.detectFromURL(url)))
-.then(responses => responses.forEach(x => col.add(x.addon)))
+.then(function(responses) {
+    responses.forEach(function(x, i) {
+       if (!x.addon) return
+        x.addon.flags = i === 0 ? { official: true, protected: true } : { official: true }
+        col.add(x.addon)
+    })
+})
 .then(function() {
     console.log(JSON.stringify(col.save(), null, 4))
 })

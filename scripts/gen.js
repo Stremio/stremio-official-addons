fc925d4a9a00c3302685d7f3608ae1182c838a14
nklhrstv <me@nikolahristov.com>
2020-09-09 16:27:48 +0300

    remove addons that does not impl addon v4 protocol

--- scripts/gen.js
+++ scripts/gen.js
@@ -3,12 +3,12 @@
 const client = require('stremio-addon-client')
 
 const ENDPOINTS = [
-    "https://v3-cinemeta.strem.io/manifest.json",
-    "https://v3-channels.strem.io/manifest.json",
-    "https://watchhub.strem.io/manifest.json",
-    "https://caching.stremio.net/publicdomainmovies.now.sh/manifest.json",
-    "https://opensubtitles.strem.io/stremio/v1",
-    "http://127.0.0.1:11470/local-addon/manifest.json",
+    "https://v4-cinemeta.strem.io/manifest.json"
+    // "https://v3-channels.strem.io/manifest.json",
+    // "https://watchhub.strem.io/manifest.json",
+    // "https://caching.stremio.net/publicdomainmovies.now.sh/manifest.json",
+    // "https://opensubtitles.strem.io/stremio/v1",
+    // "http://127.0.0.1:11470/local-addon/manifest.json",
 ]
 
 const col = new client.AddonCollection()

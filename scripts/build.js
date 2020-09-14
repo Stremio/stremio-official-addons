a3baf26de9161056873c83d5921156983fb8aefa
nklhrstv <me@nikolahristov.com>
2020-09-14 17:01:51 +0300

    all official addons added to build script

--- scripts/build.js
+++ scripts/build.js
@@ -2,25 +2,28 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
 const validator = require('@stremio/stremio-core-validator');
+const legacyManifestMapper = require('stremio-addon-client/lib/transports/legacy/mapper');
 
+const LEGACY_REQUEST_PARAM = '/q.json?b=eyJwYXJhbXMiOltdLCJtZXRob2QiOiJtZXRhIiwiaWQiOjEsImpzb25ycGMiOiIyLjAifQ==';
 const PROTECTED_URLS = [
     "https://v4-cinemeta.strem.io/manifest.json",
-    // "http://127.0.0.1:11470/local-addon/manifest.json",
+    "http://127.0.0.1:11470/local-addon/manifest.json",
 ];
 const OFFICIAL_URLS = [
-    "https://v4-cinemeta.strem.io/manifest.json"
-    // "https://v3-channels.strem.io/manifest.json",
-    // "https://watchhub.strem.io/manifest.json",
-    // "https://caching.stremio.net/publicdomainmovies.now.sh/manifest.json",
-    // "https://opensubtitles.strem.io/stremio/v1",
-    // "http://127.0.0.1:11470/local-addon/manifest.json",
+    "https://v4-cinemeta.strem.io/manifest.json",
+    "https://v3-channels.strem.io/manifest.json",
+    "https://watchhub.strem.io/manifest.json",
+    "https://caching.stremio.net/publicdomainmovies.now.sh/manifest.json",
+    "https://opensubtitles.strem.io/stremio/v1",
+    "http://127.0.0.1:11470/local-addon/manifest.json",
 ];
 
 function getDescriptor(transportUrl) {
-    return fetch(transportUrl)
+    const legacy = transportUrl.endsWith('stremio/v1');
+    return fetch(legacy ? `${transportUrl}${LEGACY_REQUEST_PARAM}` : transportUrl)
         .then((resp) => resp.json())
-        .then((manifest) => ({
-            manifest,
+        .then((resp) => ({
+            manifest: legacy ? legacyManifestMapper.mapManifest(resp.result) : resp,
             transportUrl,
             flags: {
                 official: true,

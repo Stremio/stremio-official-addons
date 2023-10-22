89ed1826094e02a0f6bcce89842442bf663340fc
unclekingpin <unclekingpin@proton.me>
2023-10-22 01:02:25 -0700

    update subtitles addon

--- scripts/build.js
+++ scripts/build.js
@@ -16,7 +16,7 @@ const OFFICIAL_URLS = [
     'https://v3-channels.strem.io/manifest.json',
     'https://watchhub.strem.io/manifest.json',
     'https://caching.stremio.net/publicdomainmovies.now.sh/manifest.json',
-    'https://opensubtitles.strem.io/stremio/v1',
+    'https://opensubtitles-v3.strem.io/manifest.json',
     'http://127.0.0.1:11470/local-addon/manifest.json',
 ];
 

f3e2c1d4122ebc45dc6c6a7839cfd5b5ae81e36b
Lachezar Lechev <lachezar@ambire.com>
2024-01-10 13:45:38 +0200

    feat: parse json to serde_json::Value with feature
    Signed-off-by: Lachezar Lechev <lachezar@ambire.com>

--- official-addons-v2/src/lib.rs
+++ official-addons-v2/src/lib.rs
@@ -1,1 +1,27 @@
+#[cfg(feature = "json")]
+pub use serde_json::Value;
+#[cfg(feature = "json")]
+use once_cell::sync::Lazy;
+
+/// The JSON file's content
 pub const ADDONS: &'static [u8] = include_bytes!("../addons.json");
+
+#[cfg(feature = "json")]
+/// The JSON file content but after json validation
+pub const ADDONS_JSON: Lazy<Value> =
+    Lazy::new(|| serde_json::from_slice(ADDONS).expect("Valid JSON"));
+
+// todo: parsed addons manifests
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    #[cfg(feature = "json")]
+    fn addons_file_is_valid_json() {
+        let addons_json = ADDONS_JSON.clone();
+
+        assert_ne!(addons_json, serde_json::Value::Null);
+    }
+}

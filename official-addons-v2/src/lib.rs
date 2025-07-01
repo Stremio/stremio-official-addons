99ac776084de3449f896c515eae024532a243dfb
AlvinHV <AlvinHV@proton.me>
2025-07-02 00:56:38 +0400

    fix!: Remove AddonJson and keep as string only
    I researched and could not find reference to ADDON_JSON. Only ADDONS have been used by stremio-core which i replaced by get_addons_string function that returns either static plain text string or dynamically on demand decompressed string that deallocated after use.

--- official-addons-v2/src/lib.rs
+++ official-addons-v2/src/lib.rs
@@ -1,46 +1,30 @@
-#[cfg(all(feature = "json", feature = "deflate", not(feature = "raw_bytes")))]
+#[cfg(feature = "deflate")]
 use flate_macro::decompress;
-#[cfg(all(feature = "json", feature = "raw_bytes", not(feature = "deflate")))]
-use flate_macro::flate_static;
-
-#[cfg(feature = "json")]
-use once_cell::sync::Lazy;
-#[cfg(feature = "json")]
-pub use serde_json::Value;
+use std::borrow::Cow;
 
 /// The JSON file's content
-#[cfg(all(feature = "raw_bytes", not(feature = "deflate")))]
-pub const ADDONS: &'static [u8] = include_bytes!("../addons.json");
+#[cfg(not(feature = "deflate"))]
+const ADDONS: &'static [u8] = include_bytes!("../addons.json");
 
-#[cfg(all(feature = "raw_bytes", feature = "deflate"))]
-flate_static!(pub static ADDONS: [u8] from "../addons.json");
+pub fn get_addons_string() -> Cow<'static, [u8]> {
+    #[cfg(not(feature = "deflate"))]
+    return Cow::Borrowed(ADDONS);
 
-/// The JSON file content but after json validation
-#[cfg(all(feature = "json", feature = "raw_bytes", not(feature = "deflate")))]
-pub const ADDONS_JSON: Lazy<Value> =
-    Lazy::new(|| serde_json::from_slice(ADDONS).expect("Valid JSON"));
-
-#[cfg(all(feature = "json", feature = "deflate", not(feature = "raw_bytes")))]
-// Decompress from slice and discard it from memory
-pub const ADDONS_JSON: Lazy<Value> = Lazy::new(|| {
-    serde_json::from_slice(decompress!("addons.json").as_slice()).expect("Valid JSON")
-});
-
-#[cfg(all(feature = "json", feature = "raw_bytes", feature = "deflate"))]
-/// The JSON file content but after json validation
-pub const ADDONS_JSON: Lazy<Value> =
-    Lazy::new(|| serde_json::from_slice(&ADDONS).expect("Valid JSON")); // ADDONS is now static
-
+    #[cfg(feature = "deflate")]
+    return Cow::Owned(decompress!("addons.json"));
+}
 // todo: parsed addons manifests
 
 #[cfg(test)]
 mod test {
     use super::*;
 
+    pub use serde_json::Value;
+
     #[test]
-    #[cfg(feature = "json")]
     fn addons_file_is_valid_json() {
-        let addons_json = ADDONS_JSON.clone();
+        let addons_json: Value =
+            serde_json::from_slice(&get_addons_string()).expect("Could not decode");
 
         assert_ne!(addons_json, serde_json::Value::Null);
     }
